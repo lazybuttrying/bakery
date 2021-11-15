e46ad2370480b3be171c4fd66b20839d7871e740
lazybuttrying <79500842+lazybuttrying@users.noreply.github.com>
2021-11-16 00:07:15 +0900

    Create delete.php

---
+++ bakery/api/menu/delete.php
@@ -0,0 +1,27 @@
+<?php
+
+include_once '../../config/Database.php';
+include_once '../../model/Menu.php';
+
+// Instantiate DB & connect
+$database = new Database();
+$db = $database->connect();
+
+// Instantiate Menu object
+$menu = new Menu($db);
+
+session_start();
+if (!isset($_SESSION['user_id'])){
+  header('Location: /index.html');
+}
+
+$menu->menu_name = $_GET['menu_name']; // 같은 행의 menu_name을 가져와야함!!
+$menu->user_id = $_SESSION['user_id'];
+
+//버튼이 사라지고 그 자리에 <p>로 성공으로 교체되어
+//같은 것을 또 삭제하려는 일이 방지 됐으면
+if ($menu->delete_one())
+    echo "성공"
+
+
+?>
