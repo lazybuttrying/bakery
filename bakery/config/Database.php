f27c3a9831d8b449b577409d54cacc3a0eae38ef
lazybuttrying <79500842+lazybuttrying@users.noreply.github.com>
2021-11-15 17:15:13 +0900

    Update Database.php

--- bakery/config/Database.php
+++ bakery/config/Database.php
@@ -1,7 +1,7 @@
 <?php
 class Database {
   private $host = 'localhost';
-  private $db_name = 'bakerydb';
+  private $db_name = 'bda';
   private $username = 'root';
   private $password = '';
   private $conn;
