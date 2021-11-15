0328889803ef3ac120efa16e3ebdab1c696c9060
lazybuttrying <79500842+lazybuttrying@users.noreply.github.com>
2021-11-16 00:02:50 +0900

    Update UserSql.php

--- bakery/model/UserSql.php
+++ bakery/model/UserSql.php
@@ -7,5 +7,9 @@ class UserSql {
   public static $INSERT_USER =  "INSERT INTO user (user_id, user_name, pwd, email) VALUES (:user_id,:user_name, :pwd, :email)";
   public static $INSERT_PAYMENT = "INSERT INTO payment (user_id) VALUES (:user_id)";
 
+  public static $UPDATE_VIEW_COUNT = "UPDATE user SET view_count=:view_count WHERE user_id=:user_id";
+
+  public static $UPDATE_PWD = "UPDATE user SET pwd=:pwd WHERE user_id=:user_id";
+
 }
 ?>
