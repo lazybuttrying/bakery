6c9613f7d1c9deb1756add505254bdd90515d3ba
lazybuttrying <79500842+lazybuttrying@users.noreply.github.com>
2021-11-16 00:05:19 +0900

    Update main.php

--- bakery/view/main.php
+++ bakery/view/main.php
@@ -25,7 +25,67 @@ if (!isset($_SESSION['user_id'])){
   <form action="/bakery/api/user/logout.php" method="post">
     <input type="submit" value="Logout" name="submit">
   </form>
+  
+  <br>
+  
+    
+  <form action="">
+  <label for="season">Choose Season:</label>
+  <select name="season">
+      <option value="spring">spring</option>
+      <option value="summer">summer</option>
+      <option value="fall">fall</option>
+      <option value="winter">winter</option>
+  </select>
+  <input type="submit" value="Send" name="submit">
+  </form>
+
+
+  <p id="result">
+    <li>
+  </p>
+  
+  <br>
+  
+  
+  <form id="edit_pwd" method="post">   
+    <span> 비밀번호 수정이 성공하면 로그인 화면으로<br> 실패하면 해당 화면에 그대로 있게 됩니다. </span>
+    <br>
+    <label for="pwd"> Password </label>
+    <input type="text" name="pwd" /> <br>
+    <label for="pwdcheck"> same Password </label>
+    <input type="text" name="pwdcheck" /> <br>
+    <button type="submit" value="Edit" onclick="edit_pwd()"> Edit </button>
+  </form>
+
 
 </body>
 
+ <script>
+  $('#edit_pwd').bind('submit', (e) => {
+      e.preventDefault();
+  });
+
+  function edit_pwd() {
+    $.ajax({
+      url: "/bakery/api/user/edit_pwd.php",
+      type: "GET",
+      data: $('#edit_pwd').serialize(),
+      processData: false,
+      contentType: false,
+      cache: false,
+      timeout: 600000,
+      beforeSend: function (xhr) {
+        // pwd != pwdcheck라면 xhr.abort()
+        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded")
+      },
+      success: function (msg) {
+      },
+      error: function (e) {
+        console.log(e);
+      }
+    })
+  }
+  </script>
+  
 </html>
