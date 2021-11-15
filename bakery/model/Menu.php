30b80f8344f2c3d07b734f212ccbefe055b2137e
lazybuttrying <79500842+lazybuttrying@users.noreply.github.com>
2021-11-16 00:08:10 +0900

    Create Menu.php

---
+++ bakery/model/Menu.php
@@ -0,0 +1,67 @@
+<?php
+
+include_once 'MenuSql.php';
+
+class Menu {
+
+  // DB stuff
+  private $conn;
+  //private $table = 'menu';
+
+  // Menu Properties
+  public $menu_id;
+  public $user_id;
+  public $category;
+  public $menu_name;
+  public $price;
+
+  public select_one_by_id($menu_id) {
+    $stmt = $this->conn->prepare(MenuSql::$SELECT_ONE);  
+    $stmt->bindParam(':menu_id', $menu_id);     
+    if ($stmt->execute()){
+      if ($stmt->rowCount() == 0){
+        return False;
+      }
+      $row = $stmt->fetch(PDO::FETCH_ASSOC);
+      $this->menu_id = $row['menu_id'];
+      $this->user_id = $row['user_id'];
+      $this->category = $row['category'];
+      $this->menu_name = $row['menu_name'];
+      $this->price = $row['price'];
+      return true;
+    }
+    echo "Error: ".$stmt->error;
+    return False;
+  }
+
+  public select_all() {
+    $stmt = $this->conn->prepare(MenuSql::$SELECT_ALL);
+    $stmt->bindParam(':user_id' $this->user_id); 
+    $stmt->execute();
+    return $stmt
+  }
+
+  public delete_one() {
+    $stmt = $this->conn->prepare(MenuSql::$DELETE_ONE);
+    $stmt->bindParam(':menu_name' $this->menu_name); 
+    $stmt->bindParam(':user_id' $this->user_id); 
+    if ($stmt->execute())
+      return true;
+    echo "Error: ".$stmt->error;
+    return false;
+  }
+
+  public insert_menu() {
+    $stmt = $this->conn->prepare(MenuSql::$DELETE_ONE);
+    $stmt->bindParam(':menu_name' $this->menu_name); 
+    $stmt->bindParam(':category' $this->category); 
+    $stmt->bindParam(':price' $this->price); 
+    $stmt->bindParam(':user_id' $this->user_id); 
+    if ($stmt->execute())
+      return true;
+    echo "Error: ".$stmt->error;
+    return false;
+  }
+
+}
+?>
