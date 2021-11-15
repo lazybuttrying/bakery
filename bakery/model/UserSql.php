12a6e4e5d4a8686587941a8cbf7d92e0ad394c79
lazybuttrying <79500842+lazybuttrying@users.noreply.github.com>
2021-11-15 17:15:53 +0900

    Update UserSql.php

--- bakery/model/UserSql.php
+++ bakery/model/UserSql.php
@@ -1,9 +1,9 @@
 <?php
 class UserSql {
-  public static $SELECT_ALL = 'SELECT u.id, u.user_id, u.user_name, u.pwd, u.email, p.pay, p.date FROM user u LEFT JOIN payment p ON u.id=p.id';
-  public static $SELECT_ONE = 'SELECT u.id, u.user_id, u.user_name, u.pwd, u.email, p.pay, p.date FROM user u LEFT JOIN payment p ON u.id=p.id WHERE u.user_id=:user_id' ;
+  public static $SELECT_ALL = 'SELECT u.id, u.user_id, u.user_name, u.pwd, u.email, u.auth, u.view_count, p.payment_date FROM user u LEFT JOIN payment p ON u.user_id=p.user_id';
+  public static $SELECT_ONE = 'SELECT u.id, u.user_id, u.user_name, u.pwd, u.email, u.auth, u.view_count, p.payment_date FROM user u LEFT JOIN payment p ON u.user_id=p.user_id WHERE u.user_id=:user_id' ;
 
-  public static $SELECT_CHECK_DUP = 'SELECT id FROM user WHERE user_id=:user_id';
+  public static $SELECT_CHECK_DUP = 'SELECT user_id FROM user WHERE user_id=:user_id';
   public static $INSERT_USER =  "INSERT INTO user (user_id, user_name, pwd, email) VALUES (:user_id,:user_name, :pwd, :email)";
   public static $INSERT_PAYMENT = "INSERT INTO payment (user_id) VALUES (:user_id)";
 
