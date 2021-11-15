c3f5a45e561d255436f6e534edfefcfa0c2cca40
lazybuttrying <79500842+lazybuttrying@users.noreply.github.com>
2021-11-15 17:18:19 +0900

    Update UserSql.php

--- bakery/model/UserSql.php
+++ bakery/model/UserSql.php
@@ -1,7 +1,7 @@
 <?php
 class UserSql {
-  public static $SELECT_ALL = 'SELECT u.id, u.user_id, u.user_name, u.pwd, u.email, u.auth, u.view_count, p.payment_date FROM user u LEFT JOIN payment p ON u.user_id=p.user_id';
-  public static $SELECT_ONE = 'SELECT u.id, u.user_id, u.user_name, u.pwd, u.email, u.auth, u.view_count, p.payment_date FROM user u LEFT JOIN payment p ON u.user_id=p.user_id WHERE u.user_id=:user_id' ;
+  public static $SELECT_ALL = 'SELECT u.user_id, u.user_name, u.pwd, u.email, u.auth, u.view_count, p.payment_date FROM user u LEFT JOIN payment p ON u.user_id=p.user_id';
+  public static $SELECT_ONE = 'SELECT u.user_id, u.user_name, u.pwd, u.email, u.auth, u.view_count, p.payment_date FROM user u LEFT JOIN payment p ON u.user_id=p.user_id WHERE u.user_id=:user_id' ;
 
   public static $SELECT_CHECK_DUP = 'SELECT user_id FROM user WHERE user_id=:user_id';
   public static $INSERT_USER =  "INSERT INTO user (user_id, user_name, pwd, email) VALUES (:user_id,:user_name, :pwd, :email)";
