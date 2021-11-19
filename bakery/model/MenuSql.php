5a7e53489c29a6027980714306d0a0e1d720385d
lazybuttrying <79500842+lazybuttrying@users.noreply.github.com>
2021-11-20 02:06:20 +0900

    Update MenuSql.php

--- bakery/model/MenuSql.php
+++ bakery/model/MenuSql.php
@@ -9,7 +9,7 @@ class MenuSql {
                                 DELETE FROM `date` WHERE order_detail_id IN (SELECT o.order_detail_id FROM `order`o LEFT JOIN `menu` m ON o.menu_id=m.menu_id WHERE m.menu_name=:menu_name AND m.user_id=:user_id);
                                 DELETE FROM `season` WHERE order_detail_id IN (SELECT o.order_detail_id FROM `order`o LEFT JOIN `menu` m ON o.menu_id=m.menu_id WHERE m.menu_name=:menu_name AND m.user_id=:user_id);
                                 DELETE FROM `order` WHERE menu_id IN (SELECT menu_id FROM `menu` WHERE menu_name=:menu_name AND `user_id`=:user_id);
-                                DELETE FROM `menu` WHERE menu_name=:menu_name AND `user_id`=:user_id; "
+                                DELETE FROM `menu` WHERE menu_name=:menu_name AND `user_id`=:user_id; ";
 
     public static $INSERT_ONE = "INSERT INTO menu (menu_name, category, price, user_id) VALUES (:menu_name, :category, :price, :user_id)";
 }
